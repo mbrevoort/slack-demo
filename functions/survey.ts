1df34378ba1c85da51065c6b0ec42b88eafbba9b
Mike Brevoort <mbrevoort@slack-corp.com>
2022-04-25 16:24:53 -0700

    strip function body for real :)

--- functions/survey.ts
+++ functions/survey.ts
@@ -2,23 +2,8 @@ import type { FunctionHandler } from "deno-slack-sdk/types.ts";
 import { SlackAPI } from "deno-slack-api/mod.ts";
 
 const nps: FunctionHandler<any, any> = async ({ inputs, token }) => {
-  const { user, score } = inputs;
-
-  // save the score
-  const client = SlackAPI(token, {}); 
-  await client.apiCall("apps.datastore.put", {
-    datastore: "nps_scores",
-    item: {
-      user,
-      score,
-      created: Date.now(),
-    },
-  });  
-
   return await {
-    outputs: {
-      confirmation: `Reportd a Net Promoter Score (NPS) of ${score}`,
-    },
+    outputs: {},
   };
 };
 
