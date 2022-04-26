51a2582f78ed402fe8e05478fe547386184de57f
Mike Brevoort <mbrevoort@slack-corp.com>
2022-04-25 17:21:23 -0700

    update score description

--- manifest.ts
+++ manifest.ts
@@ -12,8 +12,8 @@ const NpsFunction = DefineFunction({
       },
       score: {
         type: Schema.types.string,
-        description: "0 is not likely, 10 is extremely likely",
-        enum: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+        description: "How likely are you to recommend? 0 = not likely, 10 = extremely likely",
+        enum: ["10", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0"],
       },
     },
     required: ["user", "score"],
